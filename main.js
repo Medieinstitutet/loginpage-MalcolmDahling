b29968ee1b35087c10d0a02f4a75211343fbb5f0
MalcolmDahling <mac.dahling@gmail.com>
2021-11-30 15:14:46 +0100

    Create users added + other fixes

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
-//add users to localstorage
+//add users to localstorage.
 let users = [
     {username:'janne', password:'test'},
-    {username:'gunde', password:'svan'}
+    {username:'gunde', password:'svan'},
+    {username:'1', password:'1'}
 ];
-localStorage.setItem( "storageUsers", JSON.stringify(users) );
-let storageUsers = JSON.parse(localStorage.getItem('storageUsers'));
+
+
+if( JSON.parse(localStorage.getItem('storageUsers')) === null ){
+    localStorage.setItem( "storageUsers", JSON.stringify(users) ); //store users in localStorage.
+}
+
+let storageUsers = JSON.parse(localStorage.getItem('storageUsers')); //add users to a variable for use the rest of the code.
+users = JSON.parse(localStorage.getItem('storageUsers')); //add users to a variable for use the rest of the code.
+
+
+//if not already logged in, set loggedIn to false.
 let loggedIn = JSON.parse(localStorage.getItem('loggedIn'));
+if(!loggedIn){
+    loggedIn = {'loggedIn':'false', 'username':'', 'password':''};
+    localStorage.setItem( 'loggedIn', JSON.stringify(loggedIn) );
+}
 
 
 
@@ -17,6 +31,9 @@ if(loggedIn.loggedIn == 'true'){
 else{
     renderLoggedOut();
 }
+
+
+
 
 
 
@@ -40,6 +57,9 @@ function renderLoggedIn(){
 
 
 
+
+
+
 function renderLoggedOut(){
     document.getElementById('headerRight').innerHTML =
     `<div id="headerRight">
@@ -49,8 +69,6 @@ function renderLoggedOut(){
             <input type="button" value="Login" id="loginButton"></input>
         </form>
     </div>`;
-
-    document.getElementById('main').innerHTML = '<p>Welcome! You can log in in the upper right corner, there you can also create a new account.</p>';
 
     document.getElementById('loginButton').addEventListener('click', function(){
         let username = document.getElementById('loginUsername').value;
@@ -67,7 +85,53 @@ function renderLoggedOut(){
         }
     
         if(loggedIn.loggedIn == 'false'){
-            alert('Wrong username or password.');
+            document.getElementById('main').innerHTML = '<p>Wrong username or password.</p>';
         }
     });
+
+
+
+
+
+    document.getElementById('main').innerHTML = `
+        <p>Welcome! You can log in in the upper right corner, or you can create a new account here:</p>
+        <form>
+            <input type="text" placeholder="Username" id="createUsername"></input>
+            <input type="password" placeholder="Password" id="createPassword"></input>
+            <input type="button" value="Create" id="createButton"></input>
+        </form>`;
+
+    document.getElementById('createButton').addEventListener('click', function(){
+
+        let createUsername = document.getElementById('createUsername').value;
+        let createPassword = document.getElementById('createPassword').value;
+
+
+        //check if username already exists.
+        let exists = 0;
+        for(let i = 0; i < users.length; i++){
+            if(createUsername == users[i].username){
+                alert('Username already exists.');
+                exists = 1;
+            }
+        }
+
+
+        if(!createUsername || !createPassword){
+            alert('Please fill in both a username and a password.');
+        }
+
+        else if(exists == 0){
+            users.push({'username':createUsername, 'password':createPassword});
+            localStorage.setItem( "storageUsers", JSON.stringify(users) ); //store users in localStorage.
+            storageUsers = JSON.parse(localStorage.getItem('storageUsers')); //add users to a variable for use the rest of the code.
+            document.getElementById('createUsername').value = '';
+            document.getElementById('createPassword').value = '';
+            alert('Account created!');
+        }
+
+    });
+  
 }
+
+
